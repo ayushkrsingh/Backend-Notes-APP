b12d743247b7849cb722f4b26b2dab65bce14b90
ayushkrsingh <ayushks070707@gmail.com>
2026-08-02 12:40:11 +0530

    added get post method to the /notes api and pushed each one to notes[]

--- src/app.js
+++ src/app.js
@@ -2,5 +2,29 @@ const express = require('express');
 
 const app = express();   // creating server instance
 
+app.use(express.json());
+
+const notes = [];
+
+// POST /notes   ----
+
+app.post('/notes', (req, res) => {
+    console.log(req.body);
+
+    notes.push(req.body);
+    res.status(201).json({
+        message: "Note created Successfully!"
+    })
+    
+})
+
+// GET /notes    -------
+
+app.get('/notes', (req, res) => {
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes: notes
+    })
+})
 
 module.exports = app;
